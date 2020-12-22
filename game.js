fcfe8538e31962aa9478945e6079d78a30942d23
Bryan Huynh <32778577+BryanHuynh@users.noreply.github.com>
2020-12-21 23:04:47 -0700

    got the block to move

--- game.js
+++ game.js
@@ -1,28 +1,38 @@
 var canvas = document.querySelector('canvas'),
     ctx    = canvas.getContext('2d');
 const gridSquareSize = 25;
+var grid = (new Array(24)).fill().map(function(){ return new Array(10).fill("white");});
+var position = [0,0];
 fitToContainer(canvas);
-drawGrid(ctx);
 
 window.addEventListener('keydown',this.check,false);
 
 function check(e) {
 	var code = e.keyCode;
     switch (code) {
-        case 37: console.log("Left"); break; //Left key
+        case 37: move(0,-1); break; //Left key
         case 38: console.log("Up"); break; //Up key
-        case 39: console.log("Right"); break; //Right key
-        case 40: console.log("Down"); break; //Down key
-        default: console.log(code); //Everything else
+        case 39: move(0,1); break; //Right key
+        case 40: move(1,0); break; //Down key
     }
+}
+function clearSquare(y, x){
+	const context = canvas.getContext('2d');
+	context.clearRect(x * gridSquareSize + 2 , y * gridSquareSize + 2, gridSquareSize -  2 , gridSquareSize - 2);
+}
+
+function move(y, x){
+	clearSquare(position[0],position[1]);
+	position[0] += y;
+	position[1] += x;
+	drawOnGrid(position[0],position[1], "blue")
 }
 
 
 function main(){
 	console.log("game start")
-	var grid = (new Array(24)).fill().map(function(){ return new Array(10).fill("white");});
-	drawOnGrid(0, 1, grid, "blue");
-
+	drawGrid(ctx);
+	drawOnGrid(position[0], position[1], grid, "blue");
 }
 
 
@@ -43,18 +53,13 @@ function drawGrid(){
 }
 
 
-function drawOnGrid(y, x, grid, color){
-	console.log(grid)
+function drawOnGrid(y, x, color){
 	grid[y][x] = color;
 	ctx.fillStyle = color
 	ctx.fillRect(x * gridSquareSize + 2 , y * gridSquareSize + 2, gridSquareSize -  2 , gridSquareSize - 2);
 	ctx.fillStyle = "white"
 }
 
-function clearGrid(){
-	const context = canvas.getContext('2d');
-	context.clearRect(0, 0, canvas.width, canvas.height);
-}
 
 
 
